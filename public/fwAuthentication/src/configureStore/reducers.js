80f039fe4c64b858d2adc8951eb457a30ee82339
John Tucker <john@larkintuckerllc.com>
2017-08-11 08:16:35 -0400

    Remove Redux Form

--- public/fwAuthentication/src/configureStore/reducers.js
+++ public/fwAuthentication/src/configureStore/reducers.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import auth from '../ducks/auth';
 
 export default combineReducers({
   auth,
-  form: formReducer,
 });

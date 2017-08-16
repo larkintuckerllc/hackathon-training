97b1d4bbbfd3084039346b755e28d14fb5d70e6d
John Tucker <john@larkintuckerllc.com>
2017-08-16 15:21:44 -0400

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+exports.handleCreateFile = functions.database.ref('/files/{fileId}')
+  .onCreate(event => {
+    const fileId = event.params.fileId;
+    const file = event.data.val();
+    const string = `/folders/${file.parent}/children/${fileId}`;
+    const childRef =
+      event.data.ref.parent.parent
+        .child('folders').child(file.parent)
+        .child('children').child(fileId);
+    return childRef.set(true);
+  });
